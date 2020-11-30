a7dee76d4d9e06bf7a1806af90dcea069ae05cb2
yuta-nihei <niheiyuta1996@gmail.com>
2020-11-30 10:08:27 +0900

    Update: Source

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <title><?php bloginfo( 'name' ); ?><?php wp_title(); ?></title>
+
+    <link href='http://fonts.googleapis.com/css?family=Acme' rel='stylesheet' type='text/css'>
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+    <header>
+    <div class="siteinfo">
+        <div class="container">
+            <h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+            <p><?php bloginfo( 'description' ) ?></p>
+        </div>
+    </div>
+    </header>
+
+    <div class="container">
+    <?php if(have_posts()): while(have_posts()): the_post(); ?>
+
+    <article <?php post_class(); ?>>
+
+        <?php if ( is_single() ): ?>
+            <h1><?php the_title(); ?></h1>
+        <?php else: ?>
+        <h1><a href="<?php the_permalink(); ?>">
+        <?php the_title(); ?></a></h1>
+        <?php endif; ?>
+
+        <div class="postinfo">
+            <time datetime="<?php echo get_the_date( 'Y-m-d' ) ?>">
+                <i class="fa fa-clock-o"></i>
+                <?php echo get_the_date(); ?>
+            </time>
+
+            <span class="postcat">
+                <i class="fa fa-folder-open"></i>
+                <?php the_category( ', ' ); ?>
+            </span>
+        </div>
+
+        <?php the_content(); ?>
+
+        <?php if( is_single() ): ?>
+        <div class="pagenav">
+            <span class="old">
+                <?php previous_post_link( '%link', '<i class="fa fa-chevron-circle-left"></i> %title' ); ?>
+            </span>
+
+            <span class="new">
+                <?php next_post_link( '%link', '%title <i class="fa fa-chevron-circle-right"></i>' ); ?>
+            </span>
+        </div>
+        <?php endif; ?>
+
+    </article>
+
+    <?php endwhile; endif; ?>
+
+    <?php if ( $wp_query->max_num_pages > 1 ): ?>
+        <div class="pagenav">
+            <span class="old">
+                <?php next_posts_link('<i class="fa fa-chevron-circle-left"></i> 古い記事' ); ?>
+            </span>
+
+            <span class="new">
+                <?php previous_posts_link( '新しい記事 <i class="fa fa-chevron-circle-right"></i>' ); ?>
+            </span>
+        </div>
+    <?php endif; ?>
+
+    </div>
+
+    <footer>
+        <div class="container">
+            <small>Copyright &copy; <?php bloginfo( 'name' ); ?></small>
+        </div>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
